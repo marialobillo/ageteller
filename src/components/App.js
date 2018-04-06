66ad21046de4bf4f8fbdaa45ef487d177485690b
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-06 20:24:32 +0200

    component and some style

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './App.css';
 
 class App extends Component{
 
 	render(){
 		return (
-			<div>ageteller component</div>
+			<div className="App">
+				<div className="container">
+					<h2>Input Your Birthday!</h2>
+					<Form inline>
+						<FormControl type="date">
+						</FormControl>
+						{' '}
+						<Button>Submit</Button>
+					</Form>
+				</div>
+			</div>
 		);
 	}
 }
