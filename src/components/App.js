9e990b949265a6d9924ca335dc8232678057c905
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-04-08 12:39:20 +0200

    using state

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,18 @@ import './App.css';
 class App extends Component{
 	constructor(){
 		super();
+
+		this.state = {
+			newDate: '',
+			birthday: '1992-06-21'
+		}
+	}
+
+	changeBirthday(){
+		console.log(this.state);
+		this.setState({
+			birthday: this.state.newDate
+		});
 	}
 	render(){
 		return (
@@ -12,10 +24,13 @@ class App extends Component{
 				<div className="container">
 					<h2>Input Your Birthday!</h2>
 					<Form inline>
-						<FormControl type="date">
+						<FormControl
+							type="date"
+							onChange={event => this.setState({ newDate: event.target.value})}
+							>
 						</FormControl>
 						{' '}
-						<Button>Submit</Button>
+						<Button onClick={() => this.changeBirthday()}>Submit</Button>
 					</Form>
 				</div>
 			</div>
